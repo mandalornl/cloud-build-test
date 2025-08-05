2fe68f9ec669cca482de7c4b9fbd568e453dea87
Joost Berns <joost.berns@softmedia.nl>
2025-08-05 14:01:48 +0200

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+</script>
+
+<template>
+  <code style="white-space: pre-wrap">
+    {{ runtimeConfig.public }}
+  </code>
+</template>
