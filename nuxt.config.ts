30d0e281ecb89f56f954a861cb3b56cfce4c133f
Joost Berns <joost.berns@softmedia.nl>
2025-08-11 11:27:55 +0200

    Cache Cloud Build flow (wip)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
+      shortSha: process.env.SHORT_SHA,
       apiKey: process.env.FIREBASE_API_KEY,
       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
       projectId: process.env.FIREBASE_PROJECT_ID,
